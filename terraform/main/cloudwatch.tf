5b0a56dfab03d8f463dbc51631a65b8008b2261d
Aku Kolu <aku.kolu@mehilainen.fi>
2025-04-07 20:39:17 +0300

    add lambda that stops lightsail instance when network idle alarm triggers

--- terraform/main/cloudwatch.tf
+++ terraform/main/cloudwatch.tf
@@ -1,7 +1,85 @@
 # Log group for instance state events
 resource "aws_cloudwatch_log_group" "instance_state_logs" {
   name              = "/aws/events/valheim-instance-state"
-  retention_in_days = 30
+  retention_in_days = 14
+}
+
+# Create the Lambda ZIP package with bootstrap script
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  output_path = "${path.module}/stop_instance_lambda.zip"
+
+  source {
+    content  = "#!/bin/sh\naws lightsail stop-instance --instance-name \"${var.instance_name}\" --region \"${var.aws_region}\""
+    filename = "bootstrap"
+  }
+}
+
+# CloudWatch Log Group for the Lambda function
+resource "aws_cloudwatch_log_group" "stop_lambda_logs" {
+  name              = "/aws/lambda/stop-valheim-server"
+  retention_in_days = 14
+}
+
+# Lambda function to stop the Lightsail instance
+resource "aws_lambda_function" "stop_lightsail_instance" {
+  function_name = "stop-valheim-server"
+  role          = aws_iam_role.stop_instance_lambda_role.arn
+  runtime       = "provided.al2"
+  handler       = "bootstrap"
+  timeout       = 30
+  architectures = ["arm64"]
+
+  depends_on       = [aws_cloudwatch_log_group.stop_lambda_logs]
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+}
+
+# IAM role for the Lambda function
+resource "aws_iam_role" "stop_instance_lambda_role" {
+  name = "stop-valheim-lambda-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "lambda.amazonaws.com" }
+    }]
+  })
+}
+
+# IAM policy for the Lambda function
+resource "aws_iam_role_policy" "stop_instance_lambda_policy" {
+  name = "stop-valheim-policy"
+  role = aws_iam_role.stop_instance_lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "arn:aws:logs:*:*:*"
+      },
+      {
+        Effect   = "Allow"
+        Action   = "lightsail:StopInstance"
+        Resource = "arn:aws:lightsail:${var.aws_region}:*:*"
+      }
+    ]
+  })
+}
+
+# Permission for CloudWatch to invoke the Lambda
+resource "aws_lambda_permission" "allow_cloudwatch" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.stop_lightsail_instance.function_name
+  principal     = "events.amazonaws.com"
 }
 
 # EventBridge rule for instance start events
@@ -69,5 +147,9 @@ resource "aws_cloudwatch_metric_alarm" "network_idle_alarm" {
     interface   = "ens5",
     server_name = var.instance_name
   }
+
+  alarm_actions = [
+    aws_lambda_function.stop_lightsail_instance.arn
+  ]
 }
 
